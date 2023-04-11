f295792523edf5f7850189de3ca58e77bc1f839a
Роман Гирченко <romangirchenko99@mail.ru>
2023-04-11 15:06:23 +0300

    add a comment

--- 2023.04.09/4.py
+++ 2023.04.09/4.py
@@ -4,3 +4,6 @@ two_num = number // 10 % 10
 three_num = number % 10
 print(f'''Сумма цифр = {one_num + two_num + three_num}
 Произведение цифр = {one_num * two_num * three_num}''')
+
+# Сумма цифр = 9
+# Произведение цифр = 27
