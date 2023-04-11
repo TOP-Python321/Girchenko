ac98d3c96a017a332fbfbaa126c9569e87583bd4
Роман Гирченко <romangirchenko99@mail.ru>
2023-04-11 18:02:17 +0300

    add corrections

--- 2023.04.09/5.py
+++ 2023.04.09/5.py
@@ -4,10 +4,11 @@ fractional_part = input('Введите дробную часть: ')
 
 mile_in_km = 1.61
 # ИСПРАВИТЬ: f-строка отработает в такой ситуации быстрее
-mile = float(whole_part + '.' + fractional_part)
+mile = float(f'{whole_part}.{fractional_part}')
+km = round(mile * mile_in_km, 1)
 
 # ИСПРАВИТЬ: если результат округления должен быть использован где-то ещё, тогда используете функцию round(); в данном случае выгоднее воспользоваться синтаксисом f-строк
-print(f'{mile} миль = {round(mile * mile_in_km, 1)} км')
+print(f'{mile} миль = {km} км')
 
 # 547.258 миль = 881.1 км
 
