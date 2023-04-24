512e843131a8b387d46e0da4416f874f9e8a0f25
Роман Гирченко <romangirchenko99@mail.ru>
2023-04-24 18:38:37 +0300

    correction

--- 2023.04.16/1.py
+++ 2023.04.16/1.py
@@ -1,29 +1,22 @@
-num_one = input('Введите первое число: ')
-num_two = input('Введите второе число: ')
-num_three = input('Введите третье число: ')
+num_one = float(input('Введите первое число: '))
+num_two = float(input('Введите второе число: '))
+num_three = float(input('Введите третье число: '))
 sum_num = 0
 
 # ИСПРАВИТЬ здесь и далее: проверка isdecimal() избыточна, потому что будет провалена каждый раз, когда в соответствующей переменной оказывается строковое представление вещественного числа
 # КОММЕНТАРИЙ: первый коммит был лучше
 # СДЕЛАТЬ: revert commit для коммита с первыми изменениями в 1.py и затем merge (необходимо будет вручную разрешить конфликт — заодно потренируетесь):
 #  https://git-scm.com/docs/git-revert
-if num_one.isdecimal():
-    sum_num += int(num_one)
-elif float(num_one) > 0:
-    sum_num += float(num_one)
+if num_one > 0:
+    sum_num += num_one
 
-if num_two.isdecimal():
-    sum_num += int(num_two)
-elif float(num_two) > 0:
-    sum_num += float(num_two)
-
-if num_three.isdecimal():
-    sum_num += int(num_three)
-elif float(num_three) > 0:
-    sum_num += float(num_three)
-
+if num_two > 0:
+    sum_num += num_two
+    
+if num_three > 0:
+    sum_num += num_three
+    
 print(sum_num)
-
 
 # Введите первое число: 10
 # Введите второе число: -9
